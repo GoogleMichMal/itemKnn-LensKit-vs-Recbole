6df25375a239895945d6bedca3c39a8073224b86
Michael Schmidt <schmidtmichael376@gmail.com>
2024-04-16 09:43:26 +0200

    metrics results stored in variables

--- Code/main.py
+++ Code/main.py
@@ -4,8 +4,14 @@ import matplotlib.pyplot as plt
 
 result_lenskit = itemknn_lenskit()
 result_recbole = itemknn_recbole()
-print(result_lenskit)
-print(result_recbole)
+
+ndcg_lenskit = result_lenskit.ndcg
+precision_lenskit = result_lenskit.precision
+recall_lenskit = result_lenskit.recall
+
+ndcg_recbole = result_recbole['ndcg@10']
+precision_recbole = result_recbole['precision@10']
+recall_recbole = result_recbole['recall@10']
 
 
 # Plotting the results of the two algorithms side by side for ndcg, precision and recall
