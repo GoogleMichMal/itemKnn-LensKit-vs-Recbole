fa651753851c7fb263e901679cf69d1b23d52591
GoogleMichMal <94409010+GoogleMichMal@users.noreply.github.com>
2024-04-16 09:12:18 +0200

    edit_main

--- Code/itemknnrecbole.py
+++ Code/itemknnrecbole.py
@@ -1,6 +1,6 @@
 from recbole.quick_start import run_recbole
 def itemknn_recbole():
     result = run_recbole(model='ItemKNN', dataset='ml-100k', config_file_list=['Code/recbole.yaml'])
-    return recall, mrr, ndcg, precision
+    return result['test_result']
 
 itemknn_recbole()
--- Code/main.py
+++ Code/main.py
@@ -2,8 +2,9 @@ from itemknnlenskit import itemknnlenskit
 from itemknnrecbole import itemknnrecbole
 import matplotlib.pyplot as plt
 
-itemknnlenskit()
-itemknnrecbole()
+result_lenskit = itemknnlenskit()
+result_recbole = itemknnrecbole()
+
 
 # Plotting the results of the two algorithms side by side for ndcg, precision and recall
 
