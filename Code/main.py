67f9a5e5de75f38f1c04ebb4a3503edf80769448
Verix <77591428+Veriiix@users.noreply.github.com>
2024-04-16 09:49:31 +0200

    limit axis on plot

--- Code/main.py
+++ Code/main.py
@@ -17,15 +17,15 @@ recall_recbole = result_recbole['recall@10']
 # Plotting the results of the two algorithms side by side for ndcg, precision and recall
 plt.figure(figsize=(15, 5))
 plt.subplot(1, 3, 1)
-plt.bar(['Lenskit', 'RecBole'], [ndcg_lenskit, ndcg_recbole], color=['blue', 'orange'])
+plt.bar(['Lenskit', 'RecBole'], [ndcg_lenskit, ndcg_recbole], color=['blue', 'red'])
 plt.title('NDCG@10')
+plt.ylim([0, 0.5])
 plt.subplot(1, 3, 2)
-plt.bar(['Lenskit', 'RecBole'], [precision_lenskit, precision_recbole], color=['blue', 'orange'])
+plt.bar(['Lenskit', 'RecBole'], [precision_lenskit, precision_recbole], color=['blue', 'red'])
 plt.title('Precision@10')
+plt.ylim([0, 0.5]) 
 plt.subplot(1, 3, 3)
-plt.bar(['Lenskit', 'RecBole'], [recall_lenskit, recall_recbole], color=['blue', 'orange'])
+plt.bar(['Lenskit', 'RecBole'], [recall_lenskit, recall_recbole], color=['blue', 'red'])
 plt.title('Recall@10')
+plt.ylim([0, 0.5])
 plt.show()
-
-
-
