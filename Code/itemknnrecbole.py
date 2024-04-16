2cafe2da67df42beafb7ef7de1ae9f3ed2e0cb21
Verix <77591428+Veriiix@users.noreply.github.com>
2024-04-16 09:04:27 +0200

    restructure

--- Code/itemknnrecbole.py
+++ Code/itemknnrecbole.py
@@ -1,3 +1,6 @@
 from recbole.quick_start import run_recbole
+def itemknn_recbole():
+    result = run_recbole(model='ItemKNN', dataset='ml-100k', config_file_list=['Code/recbole.yaml'])
+    return recall, mrr, ndcg, precision
 
-run_recbole(model='ItemKNN', dataset='ml-100k', config_file_list=['recbole.yaml'])
+itemknn_recbole()
---
+++ Code/main.py
@@ -0,0 +1,11 @@
+from itemknnlenskit import itemknnlenskit
+from itemknnrecbole import itemknnrecbole
+import matplotlib.pyplot as plt
+
+itemknnlenskit()
+itemknnrecbole()
+
+# Plotting the results of the two algorithms side by side for ndcg, precision and recall
+
+
+
