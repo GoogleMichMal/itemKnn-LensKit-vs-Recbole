3fd5c186b4b730a85b3b96de4c790b1dbe34ac1c
Verix <77591428+Veriiix@users.noreply.github.com>
2024-04-16 09:18:31 +0200

    function

--- Code/itemknnlenskit.py
+++ Code/itemknnlenskit.py
@@ -3,28 +3,27 @@ from lenskit.datasets import ML100K
 from lenskit import batch, topn
 from lenskit.algorithms import Recommender, item_knn
 
-ml100k = ML100K('data/ml-100k')
-ratings = ml100k.ratings
-ratings.head()
 
-test = ratings.sample(frac=0.2, random_state=42)
-train = ratings.drop(test.index)
-print('Train:', train.shape, 'Test:', test.shape)
+def itemknnlenskit():
+    ml100k = ML100K("data/ml-100k")
+    ratings = ml100k.ratings
+    ratings.head()
 
-item_knn = item_knn.ItemItem(20, feedback='implicit')
+    test = ratings.sample(frac=0.2, random_state=42)
+    train = ratings.drop(test.index)
+    print("Train:", train.shape, "Test:", test.shape)
 
-fittable = Recommender.adapt(item_knn)
+    item_knn = item_knn.ItemItem(20, feedback="implicit")
 
-fittable.fit(train)
+    fittable = Recommender.adapt(item_knn)
 
-users = test.user.unique()
+    fittable.fit(train)
 
-recs = batch.recommend(fittable, users, 10, n_jobs=1)
+    users = test.user.unique()
 
-rla = topn.RecListAnalysis()
-rla.add_metric(topn.ndcg)
-rla.add_metric(topn.precision)
-rla.add_metric(topn.recall)
-results = rla.compute(recs, test)
-print(f"NDCG@10: {results.mean()}")
+    recs = batch.recommend(fittable, users, 10, n_jobs=1)
 
+    rla = topn.RecListAnalysis()
+    rla.add_metric(topn.ndcg)
+    results = rla.compute(recs, test)
+    print(f"NDCG@10: {results.mean()}")
