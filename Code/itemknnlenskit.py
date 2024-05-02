e898bbf54b58b9c38e3a8879f43d296be8e7ae14
Verix <77591428+Veriiix@users.noreply.github.com>
2024-05-02 11:43:33 +0200

    itemknn_lenskit_bookcrossing()

--- Code/itemknnlenskit.py
+++ Code/itemknnlenskit.py
@@ -29,3 +29,32 @@ def itemknn_lenskit_ml100k():
     rla.add_metric(topn.recall)
     results = rla.compute(recs, test)
     return results.mean()
+
+def itemknn_lenskit_bookcrossing():
+    bookcrossing = pd.read_csv("/Data/book-crossing/Ratings.csv", sep=";", names=["user", "item", "rating"])
+
+    test = bookcrossing.sample(frac=0.2, random_state=42)
+    train = bookcrossing.drop(test.index)
+    print("Train:", train.shape, "Test:", test.shape)
+
+    itemknn = item_knn.ItemItem(20, feedback="implicit")
+
+    train["user"] = train["user"].astype(str)
+
+    fittable = Recommender.adapt(itemknn)
+
+    fittable.fit(train)
+
+    users = test.user.unique()
+
+    recs = batch.recommend(fittable, users, 10, n_jobs=1)
+
+    rla = topn.RecListAnalysis()
+    rla.add_metric(topn.ndcg)
+    rla.add_metric(topn.precision)
+    rla.add_metric(topn.recall)
+    results = rla.compute(recs, test)
+    return results.mean()
+
+
+print(itemknn_lenskit_bookcrossing())
