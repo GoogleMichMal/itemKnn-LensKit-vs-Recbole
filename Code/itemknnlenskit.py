3cf9f5974b99dbafecacfd0b6f2a13a79b9779b9
Michael Schmidt <80267548+michi774@users.noreply.github.com>
2024-05-02 11:44:23 +0200

    itemknn lenskit book crossing

--- Code/itemknnlenskit.py
+++ Code/itemknnlenskit.py
@@ -11,7 +11,6 @@ def itemknn_lenskit_ml100k():
 
     test = ratings.sample(frac=0.2, random_state=42)
     train = ratings.drop(test.index)
-    print("Train:", train.shape, "Test:", test.shape)
 
     itemknn = item_knn.ItemItem(20, feedback="implicit")
 
@@ -29,3 +28,33 @@ def itemknn_lenskit_ml100k():
     rla.add_metric(topn.recall)
     results = rla.compute(recs, test)
     return results.mean()
+
+
+def itemknn_lenskit_bookcrossing():
+    ratings = pd.read_csv("Data/book-crossing/Ratings.csv", sep=";", names=["user", "item", "rating"])
+    ratings.head()
+
+    test = ratings.sample(frac=0.2, random_state=42)
+    train = ratings.drop(test.index)
+
+    train["user"] = train["user"].astype(str)
+    test["user"] = test["user"].astype(str)
+
+    itemknn = item_knn.ItemItem(20, feedback="implicit")
+
+    fittable = Recommender.adapt(itemknn)
+
+    fittable.fit(train)
+
+    users = test.user.unique()
+
+    recs = batch.recommend(fittable, users, 10, n_jobs=1)
+
+    rla = topn.RecListAnalysis()
+    rla.add_metric(topn.ndcg)
+    rla.add_metric(topn.precision)
+    rla.add_metric(topn.recall)
+    results = rla.compute(recs, test)
+    return results.mean()
+
+itemknn_lenskit_bookcrossing()
