d4c6f3de93e4af4b1854c37d1cdfcd186c7d0aca
Verix <77591428+Veriiix@users.noreply.github.com>
2024-05-07 12:00:55 +0200

    update download

--- Code/download-data.py
+++ Code/download-data.py
@@ -1,21 +1,31 @@
 import os
 import zipfile
 import requests
+from tqdm import tqdm
 
 
 def download_and_extract(url, target_dir):
     # Download the ZIP file
-    response = requests.get(url)
+    response = requests.get(url, stream=True)
     if response.status_code != 200:
         print(
             f"Failed to download ZIP file from {url}. Status code: {response.status_code}"
         )
         return
 
+    # Get the total file size in bytes
+    total_size = int(response.headers.get("content-length", 0))
+
+    # Initialize progress bar
+    progress_bar = tqdm(total=total_size, unit="B", unit_scale=True)
+
     # Write the downloaded content to a file
     zip_file_path = os.path.join(target_dir, "Data.zip")
     with open(zip_file_path, "wb") as zip_file:
-        zip_file.write(response.content)
+        for data in response.iter_content(chunk_size=1024):
+            progress_bar.update(len(data))
+            zip_file.write(data)
+    progress_bar.close()
 
     # Extract the contents of the ZIP file
     with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
