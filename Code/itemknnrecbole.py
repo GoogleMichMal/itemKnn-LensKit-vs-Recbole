f820406161c4e582ebf4dd3308b49b801a4810b2
GoogleMichMal <94409010+GoogleMichMal@users.noreply.github.com>
2024-05-09 10:55:34 +0200

    Update itemknnrecbole.py [not working]

--- Code/itemknnrecbole.py
+++ Code/itemknnrecbole.py
@@ -1,4 +1,47 @@
 from recbole.quick_start import run_recbole
+from recbole.config import Config
+from recbole.data.transform import construct_transform
+from recbole.data import create_dataset, data_preparation
+from recbole.model.general_recommender.itemknn import ItemKNN
+from recbole.utils import init_seed, get_model, get_trainer
+
+def runitemknn_recbole(config):
+    dataset = create_dataset(config['model'])
+    print(dataset)
+    #data splitting
+    train_data, valid_data, test_data = data_preparation(config, dataset)
+
+    #model loading
+    init_seed(config["seed"] + config["local_rank"], config["reproducibility"])
+    model = get_model(config["model"])(config, train_data._dataset).to(config["device"])
+    transform = construct_transform(config)
+
+    # trainer loading and initialization
+    trainer = get_trainer(config["MODEL_TYPE"], config["model"])(config, model)
+
+    # model training
+    best_valid_score, best_valid_result = trainer.fit(
+        train_data, valid_data, saved=True, show_progress=config["show_progress"]
+    )
+
+    # model evaluation
+    test_result = trainer.evaluate(
+        test_data, load_best_model=True, show_progress=config["show_progress"]
+    )
+    
+    result = {
+        "best_valid_score": best_valid_score,
+        "valid_score_bigger": config["valid_metric_bigger"],
+        "best_valid_result": best_valid_result,
+        "test_result": test_result,
+    }
+    return result
+
+
+cfg = Config(model='ItemKNN', dataset='ml-100k', config_file_list=['Data/ml-100k/recbole.yaml'])
+print(runitemknn_recbole(cfg))
+
+
 def itemknn_recbole_ml100k():
     result = run_recbole(model='ItemKNN', dataset='ml-100k', config_file_list=['Data/ml-100k/recbole.yaml'])
     return result['test_result']
