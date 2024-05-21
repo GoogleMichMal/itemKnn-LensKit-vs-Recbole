1855c76b2508d167f00525af3e06fff3b7a3a340
Michael Schmidt <80267548+michi774@users.noreply.github.com>
2024-05-21 15:07:24 +0200

    removed comments

--- Replication/lk_replication.py
+++ Replication/lk_replication.py
@@ -6,12 +6,8 @@ from Code.lk_partition_users import lk_partition_users
 from Code.make_implicit_movielens import make_implicit_movielens
 
 def itemknn_lenskit_ml100k():
-    # Read the MovieLens 100K dataset
     ml100k = pd.read_csv("Data/ml-100k/u.data", header=None, sep="\t", names=["user", "item", "rating", "timestamp"])
 
-    # data = make_implicit_movielens(ml100k)
-
-    # Split the data into training and test sets
     train, test = lk_partition_users(ml100k)
     print("Train set: ", train)
     print("Test set: ", test)
@@ -26,13 +22,11 @@ def itemknn_lenskit_ml100k():
 
     recs = batch.recommend(fittable, users, 10, n_jobs=1)
 
-    # Calculate nDCG
     rla = topn.RecListAnalysis()
     rla.add_metric(ndcg)
     rla.add_metric(precision)
     rla.add_metric(recall)
     results = rla.compute(recs, test)
-
 
     return results.mean()
 
